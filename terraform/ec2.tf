c4fdfe5a0bda60a7e4b17a42adad3bcc1ec7b538
Viktor Varaljai <varaljaivikotr@gmail.com>
2025-04-10 11:18:11 +0200

    Fix frontend port mapping and update Ansible/Terraform configs

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -26,9 +26,17 @@ resource "aws_security_group" "main" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
